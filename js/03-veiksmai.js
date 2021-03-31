47a3db2c50113f41e7cd67ea8f5255712f11cd57
Aldas <aldevinas.kasputis@gmail.com>
2021-04-01 00:02:34 +0300

    veiksmai

---
+++ js/03-veiksmai.js
@@ -0,0 +1,44 @@
+const nr1 = 8;
+const nr2 = 5;
+const nr3 = 3;
+const nr4 = 9;
+const nr5 = 6;
+
+const list = [1, 2, 3, 4, 5];
+const listVerte = list[0] + list[4];
+console.log(listVerte);
+
+
+
+const sum = nr1 +nr2;
+console.log(sum);
+
+const minus = nr1 - nr2;
+console.log(minus);
+
+const divide = nr1 / nr2;
+console.log(divide);
+
+const combo = (nr1 + nr2) * (10/nr2);
+console.log(combo);
+
+const eiliskumas = 1 + 2 - 4 * 5 / 10;
+console.log(eiliskumas);
+
+const firstName = 'Vardenis';
+const lastName = 'Pavardenis';
+
+const fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+const sayHi = 'Labas ' + firstName + '!';
+console.log(sayHi);
+
+const FirstName = 'Jonas';
+console.log(FirstName);
+
+const verte = nr1 - nr2 + nr3 - nr4 + nr5;
+console.log(verte);
+
+const sarasas = firstName + ', ' + lastName;
+console.log(sarasas);
