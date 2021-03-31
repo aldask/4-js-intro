c73c37d736d515c511e13364bfa2e1e046ddfbed
Aldas <aldevinas.kasputis@gmail.com>
2021-04-01 00:03:38 +0300

    strings'ai

---
+++ js/04-strings.js
@@ -0,0 +1,22 @@
+/*
+*/
+
+const vardas = 'Petras';
+console.log(vardas);
+
+const pavarde = "Petraitis";
+console.log(pavarde);
+
+const kabute1 = "cia yra viena (') kabute.";
+console.log(kabute1);
+
+const marke = 'Tesla';
+const model = 'S';
+const battery = 100;
+const batteryUnits = 'kW';
+
+const car = 'I wish I had ' + marke + ' model ' + model + ' with a ' + battery + batteryUnits + '.';
+console.log(car);
+
+const backtick = `I wish I had ${marke} model ${model} with a ${battery}${batteryUnits}.`
+console.log(backtick); 
